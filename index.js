35152e28c733ae5acce8489cf329ceb651c99c28
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-26 05:43:46 +0000

    completed the recursive function 'sumItemsInArray'

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+
+  let count = 0;
+
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      count += sumItems(item);
+    } else {
+      count += item;
+    }
+  });
+  return count;
 }
 
+
+
+console.log('finalValue = ',sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log('finalValue = ',sumItems([1, 2, 3, [4, 5]]));
 module.exports = sumItems;
+
